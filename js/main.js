ec5012514dc19837b3e037757b8c817fc5f96743
2016gary <grrbr@sina.com>
2017-07-20 13:35:58 +0800

    修改bug

--- js/main.js
+++ js/main.js
@@ -135,15 +135,12 @@
 	}
 
 	function uploadPic(data) {
-		// dataURL��ʽΪ
-		// ��data:image/png;base64,****��,����֮ǰ����˵�������֣�ֻ��Ҫ����֮��
 		data = data.split(',')[1];
 		data = window.atob(data);
 		var ia = new Uint8Array(data.length);
 		for (var i = 0; i < data.length; i++) {
 			ia[i] = data.charCodeAt(i);
 		}
-		// canvas.toDataURL ���ص�Ĭ�ϸ�ʽ���� image/png
 		var blob = new Blob([ ia ], {
 			type : "image/png"
 		});
